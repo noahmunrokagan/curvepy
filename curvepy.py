afc34cde1748f8f6b279c895d78487838d28cb12
Noah Munro-Kagan <noah.munro-kagan@caurtech.com>
2026-01-04 19:03:47 -0800

    add methods to class

--- curvepy.py
+++ curvepy.py
@@ -112,8 +112,84 @@ class CurveletFrequencyGrid():
         self.Slopes_EW = self.Y / self.X
         self.Slopes_NS = self.X / self.Y
         self.Quadrants = {
-            "East": (self.X > 0) & (np.abs(self.Y) <= self.X)
+            "East": (self.X > 0) & (np.abs(self.Y) <= self.X),
+            "North": (self.Y < 0) & (np.abs(self.X) <= self.Y),
+            "West": (self.X < 0) & (np.abs(self.Y) <= self.X),
+            "South": (self.Y > 0) & (np.abs(self.X) <= self.Y)
         }
+
+    def _get_corona_indices(self, scale_idx: int):
+        """
+        Calculates slice indices for a specific frequency scale
+
+        
+        # INPUTS:
+        # N             -> Integer, dimension of the image, must be power of 2 (e.g., 512)
+        # scale_idx     -> Integer, index of concentric shells, 0 is coarsest (center), 
+        #                  total_scales-1 is finest (border)
+        # total_scales  -> Integer, total number of concentric shells
+
+        # OUTPUTS:
+        # row_indices   -> Tuple (start, end) for the rows
+        # col_indices   -> Tuple (start, end) for the columns
+        """
+        if not is_power_of_two(self.N):
+            raise ValueError("Input image dimension must be a power of 2")
+        total_scales = len(self.scales)
+        center = int(np.floor(self.N, 2))
+
+        base_radius = int(np.floor(self.N, (2 ** total_scales))) # center to outside of shell
+
+        if scale_idx == 0:
+            radius = base_radius
+        else:
+            radius = base_radius * (2 ** scale_idx)
+
+        # Compute boundaries
+        row_start = center - radius
+        row_end = center + radius
+        col_start = center - radius
+        col_end = center + radius
+
+        # Ensure no invalid indices
+        if (row_start < 0 or col_start < 0) or (row_end > self.N or col_end > self.N):
+            raise ValueError("An unknown error occured, radius of value: {radius} incompatible with image with dimensions: {N}")
+        
+        
+        return (row_start, row_end), (col_start, col_end)
+
+
+    def _get_corona_mask(self, scale_idx: int, total_scales: int) -> np.ndarray:
+        """
+        Creates a binary mask (1 inside the shell, 0 outside) for visualization.
+
+        # INPUTS:
+        # N             -> Integer, dimension of the image, must be power of 2 (e.g., 512)
+        # scale_idx     -> Integer, index of concentric shells, 0 is coarsest (center), 
+        #                  total_scales-1 is finest (border)
+        # total_scales  -> Integer, total number of concentric shells
+
+        # OUTPUTS:
+        # mask          -> np.ndarray, 2D array visualizing the shell computed
+        """
+        
+        # Initialize mask of zeros
+
+        mask = np.zeros((self.N,self.N))
+
+        # Get outer square coordinates
+        outer_rows, outer_cols = self._get_corona_indices(scale_idx)
+
+        # Fill outer square
+        mask[outer_rows[0]:outer_rows[1], outer_cols[0]:outer_cols[1]] = 1
+    
+        # Handle hole if present
+        if scale_idx > 0:
+            inner_rows, inner_cols = self._get_corona_indices(self.scale_idx - 1)
+
+            mask[inner_rows[0]:inner_rows[1], inner_cols[0]:inner_cols[1]] = 0
+
+        return mask
 
 
         
